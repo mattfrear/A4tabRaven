d91778d6700b04208824a901820dc9c6b92e9622
mattfrear <matt.frear@gmail.com>
2013-03-19 21:39:00 +0000

    jquery mobified list and details pages

--- Web/Extensions/MyHtmlExtensions.cs
+++ Web/Extensions/MyHtmlExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
 namespace Web.Extensions
 {
@@ -10,10 +11,20 @@ namespace Web.Extensions
     {
         public static IHtmlString UniqueStartChar(this HtmlHelper helper, string word, ref string currentStartChar)
         {
+            string openTag = "<h4>";
+            string closeTag = "</h4>";
+
+            var isMobile = helper.ViewContext.HttpContext.GetOverriddenBrowser().IsMobileDevice;
+            if (isMobile)
+            {
+                openTag = "<li data-role=\"listdivider\">";
+                closeTag = "</li>";
+            }
+
             if (!word.StartsWith(currentStartChar, StringComparison.OrdinalIgnoreCase))
             {
                 currentStartChar = word.Substring(0, 1).ToUpper();
-                return MvcHtmlString.Create("<h4>" + currentStartChar + "</h4>");
+                return MvcHtmlString.Create(openTag + currentStartChar + closeTag);
             }
 
             return MvcHtmlString.Empty;
